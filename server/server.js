78f2d312abcdc6b1172d9f026ad039ad63aa47da
Carlos Rodrigo <crodrigoquero@MacBook-Air.home>
2018-08-28 22:03:27 +0100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post('/todos', (req, res) => { // entrada de datos desde el cliente
     //console.log(req.body);
     var todo = new Todo({
         text: req.body.text
@@ -22,7 +22,13 @@ app.post('/todos', (req, res) => {
     });
 });
 
-
+app.get('/todos', (req, res) =>{ // salida de datos hacia el cliente
+    Todo.find().then((todos) => {
+      res.send({todos});  
+    })
+}, (e) => {
+    res.status(400).send(e);
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
