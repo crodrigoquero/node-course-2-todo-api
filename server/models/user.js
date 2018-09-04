694756871b67c49dbbd7293300edade0c12996a7
Carlos Rodrigo <crodrigoquero@MacBook-Air.home>
2018-09-04 22:48:27 +0100

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,63 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
+// any changes in the models below, requires drop database and server to be restarted /reinitiated
+var UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
         trim: true,
-        minLength: 1
-    }
+        minLength: 1,
+        unique: true,
+        validate: {
+            validator: (value) => { // you can set this prop to "validator.isEmail" for short
+                // here we need 3th party library to perform the email validation!
+                return validator.isEmail(value);
+            },
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            require: true
+        },
+        token: {
+            type: String,
+            require: true
+        }
+    }]
 });
+
+// we need to override the method 'toJSON' in order to set the
+// properties we want to send back to the client app, because we don't want to 
+// to send the hash values at all
+UserSchema.methods.toJSON = function () { // we need o use  regular funtion (not arrow function)
+    var user = this;
+    var userObject = user.toObject();
+
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    user.tokens = user.tokens.concat({access, token}); //user.tokens.push({access, token});
+
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
