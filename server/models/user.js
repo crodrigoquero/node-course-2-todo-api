a763565b2c45a34a92842f9870ea59c54a2bbd1c
Carlos Rodrigo <crodrigoquero@MacBook-Air.home>
2018-09-13 14:32:17 +0100

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -61,6 +61,16 @@ UserSchema.methods.generateAuthToken = function () { // UserSchema.methods, is a
     });
 };
 
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+
+    return user.update({
+        $pull: { // $pull is a awsome mongo db apoerator, removes  an element from an array that matches certain criteria...
+            tokens: {token}
+        }
+    });
+};
+
 UserSchema.statics.findByToken = function(token) {
     var User = this;
     var decoded;
